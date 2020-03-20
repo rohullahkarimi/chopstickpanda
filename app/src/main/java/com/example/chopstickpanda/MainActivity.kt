664454761f4ded29e75a365bfb8a5ff996ad4b21
Rohullah Karimi <rohullahahmad99@gmail.com>
2020-03-20 14:19:59 +0200

    homepage is created

--- app/src/main/java/com/example/chopstickpanda/MainActivity.kt
+++ app/src/main/java/com/example/chopstickpanda/MainActivity.kt
@@ -1,12 +1,26 @@
 package com.example.chopstickpanda
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var button:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnOpenActivity : Button = findViewById(R.id.order)
+        btnOpenActivity.setOnClickListener {
+            val intent = Intent(this, ravintola_sivu :: class.java)
+            // start your next activity
+            startActivity(intent)
+        }
     }
+
+
 }
